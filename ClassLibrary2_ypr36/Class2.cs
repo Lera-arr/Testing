8cf3b731e84f1e545d1402424b4ec5d0064985c7
Lera-arr <lerok-mazurenko@yandex.ru>
2022-11-18 20:34:54 +0300

    Тест 3

--- ClassLibrary2_ypr36/Class2.cs
+++ ClassLibrary2_ypr36/Class2.cs
@@ -11,17 +11,28 @@ namespace ClassLibrary2_ypr36
 {
     [TestFixture]//создание теста
     public class Class2
+   
+
     {
         IWebDriver webDriver = new ChromeDriver(); //создание и инициализации на браузер
 
         [TestCase]//Тестовый случай
         public void yaru_reguest()
         {
-            webDriver.Url = "https://odisseya-tur.com/"; //установка адреса и переход в браузер
+            webDriver.Url = "https://ya.ru/"; //установка адреса и переход в браузер
+
             IWebElement search = webDriver.FindElement(By.XPath("//*[@id=\"text\"]"));
+
             search.SendKeys("Туры по России");
-            IWebElement button = webDriver.FindElement(By.XPath("/html/body/table/tbody/tr[3]/td[1]/table[1]/tbody/tr[4]/td/a "));
+
+            IWebElement button = webDriver.FindElement(By.XPath("/html/body/main/div[2]/form/div/button"));
+
             button.Click();
+
+
         }
+
+
     }
+
 }
---
+++ ClassLibrary2_ypr36/Class3.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2_ypr36
+{
+    internal class Class3
+    {
+        using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2_ypr36
+    {
+        [TestFixture]//создание теста
+        public class Class2
+
+
+        {
+            IWebDriver webDriver = new ChromeDriver(); //создание и инициализации на браузер
+
+            [TestCase]//Тестовый случай
+            public void yaru_reguest()
+            {
+                webDriver.Url = "https://ya.ru/"; //установка адреса и переход в браузер
+
+                IWebElement search = webDriver.FindElement(By.XPath("//*[@id=\"text\"]"));
+
+                search.SendKeys("Туры по России");
+
+                IWebElement button = webDriver.FindElement(By.XPath("/html/body/main/div[2]/form/div/button"));
+
+                button.Click();
+
+
+
+                IWebElement ccl = webDriver.FindElement(By.XPath("//*[@id=\"search-result\"]/li[1]/div/div[1]/a/h2/span"));
+
+                button.Click();
+
+            }
+
+
+        }
+
+    }
+
+}
+}
