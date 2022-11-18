1ce72dad487a46a19f95a896efa26932d73e27e8
Lera-arr <lerok-mazurenko@yandex.ru>
2022-11-18 17:21:36 +0300

    Проверка заголовка страницы

--- ClassLibrary2_ypr36/Class1.cs
+++ ClassLibrary2_ypr36/Class1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,23 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary2_ypr36
 {
+    [TestFixture]//создание теста
     public class Class1
     {
+        IWebDriver webDriver = new ChromeDriver(); //создание и инициализации на браузер
+
+        [TestCase]//Тестовый случай
+        public void maintTitle()
+        {
+            webDriver.Url = "https://odisseya-tur.com/"; //установка адреса и переход в браузер
+            Assert.AreEqual("Турагентство Одиссея Рязань - туристическое агентство, турфирма", webDriver.Title);// Предположение,что заловок сайта равен указанной строки
+            webDriver.Close(); //закрытие окна браузера
+        }
+
+        [TearDown]//метод, который вызывают после всех тестов
+        public void testEnd()
+        { //освобождение ссылки на браузер
+            webDriver.Quit();
+        }
     }
 }
